edbd62d522a08591f72cacad0e846f8f8fb057e1
David LACOURT <david.lacourt@gmail.com>
2019-10-31 21:40:11 +0100

    Updated to newer stencil version, applying suggestions provided by the kind compiler <3

---
+++ src/components/qr-code/enums.ts
@@ -0,0 +1,13 @@
+
+export enum OutputMode {
+  DataURI = "DataURI",
+  SVG = "SVG",
+  Table = "Table"
+}
+
+export enum ErrorCorrectionLevel {
+  Low = "L",
+  Medium = "M",
+  Quality = "Q",
+  High = "H"
+}
