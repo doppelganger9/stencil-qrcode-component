9e2b083b3d01a83d491290c70436e5575930f0c6
David LACOURT <david.lacourt+github@gmail.com>
2023-02-10 00:26:25 +0100

    Enable and rewrite as MDX story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,12 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-docs"
   ],
   "framework": "@storybook/html"
 }
